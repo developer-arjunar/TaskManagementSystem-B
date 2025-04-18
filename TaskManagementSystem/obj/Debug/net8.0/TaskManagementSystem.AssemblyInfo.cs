d7e289d8ba8c03bd94a1bc26d84e02cc2a2651fc
developer-arjunar <arjuna.r@coopinsu.com>
2025-04-18 10:58:57 +0530

    25/04/18_I

--- TaskManagementSystem/obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
+++ TaskManagementSystem/obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528dedcab32a0f3a965d9e797784561269ded458")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf6f462bf3766abedd2bcb07d88570cc3d2ee57")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
